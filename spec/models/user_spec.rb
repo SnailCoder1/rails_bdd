0d1bcff06282a9154610d70dc721e6447eab7853
boa <boa.matule@gmail.com>
2019-04-28 19:48:16 +0200

    added validates_presence_of email and password for the user added conditions for user validations in RSpec. all green.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'DB table' do
+    it { is_expected.to have_db_column :email }
+    it { is_expected.to have_db_column :encrypted_password }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_presence_of :encrypted_password } 
+  end
 end
